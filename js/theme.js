ada44f27f0da5c013f1f510f3b4a6f75cbac3312
Mahshid Alinoori <mahshid.alinoori@gmail.com>
2021-09-30 00:31:54 -0400

    config3 samples

--- js/theme.js
+++ js/theme.js
@@ -23,3 +23,11 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
